10b9ba91264b0b6e31f1771f8e45efbe561443a6
nileshgadgi <gadginileshp@gmail.com>
2023-08-25 19:40:53 +0530

    fix: Follow CD standards and remove unneccesary dependencies

--- _examples/basic/example.tf
+++ _examples/basic/example.tf
@@ -2,30 +2,29 @@
 # Managed By : CloudDrove 
 # Copyright @ CloudDrove. All Right Reserved.
 
-# ------------------------------------------------------------------------------
-# Provider
-# ------------------------------------------------------------------------------
+##------------------------------------------------------------------------------
+## Provider
+##------------------------------------------------------------------------------
 provider "aws" {
   region = "us-east-1"
 }
 
-# ------------------------------------------------------------------------------
-# AWS Athena Module
-# ------------------------------------------------------------------------------
+##------------------------------------------------------------------------------
+## AWS Athena Module
+##------------------------------------------------------------------------------
 
 module "athena" {
-  source = "../../"
-
-  name        = "athena"
-  environment = "test"
-  label_order = ["name", "environment"]
-
+  source                  = "../../"
+  name                    = "athena"
+  environment             = "test"
+  label_order             = ["name", "environment"]
   enabled                 = true
   workgroup_force_destroy = true
 
   # S3 Bucket Configuration
   bucket_force_destroy = true
   s3_output_path       = "accessLogs/queryresults/" # The S3 bucket path used to store query results
+  bucket_versioning    = true
 
   # Database for Athena
   databases = {
